35ab308da491557a1832bc3efd380670cd0f28b2
SZYZKJ <15622146998@163.com>
2020-07-21 05:47:59 +0800

    update .gitignore

--- h5.sh
+++
@@ -1,4 +0,0 @@
-sudo rm -rf h5
-unzip h5.zip
-sudo rm -rf __MACOSX/
-sudo rm -rf h5.zip
--- wx_ip_list/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@Author  : monabai
-@Time    : 2018/8/22 16:26
-@Software: PyCharm
-@File    : __init__.py.py
-"""
-
-if __name__ == '__main__':
-    pass
